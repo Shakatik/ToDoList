befb2862da4085c0ccacdad4feec8f35f28553df
Shakatik <kiberking11@gmail.com>
2023-05-25 14:39:09 +0300

    Update script.js

--- ToDo/script.js
+++ ToDo/script.js
@@ -1,12 +1,14 @@
 const filterButtons = document.querySelectorAll(".button__filter");
+const taskCount = document.querySelector(".task__count")
 const input = document.querySelector("input");
 const taskList = document.querySelector(".task__list");
-const checkAll = document.querySelector(".check__all")
+const checkAll = document.querySelector(".check__all");
 const completeCleanButton = document.getElementById("allCompleteCleanButton");
+let filterType = localStorage.getItem("filter-type") || "all__item";
 let initialData = JSON.parse(localStorage.getItem("array")) || [];
 let renderData = [...initialData];
 let objectId = parseInt(localStorage.getItem("objectId")) || 0;
-// let isChecked = localStorage.getItem("isChecked");
+filterItemsByType(filterType);
 render();
 
 function render() {
@@ -16,56 +18,56 @@ function render() {
   });
 
   renderData.forEach((element) => {
-    console.log(element)
+    console.log(element);
     const completeToggle = document.createElement("button");
     completeToggle.classList.add("complete__toggle");
     if (element.isChecked) {
       completeToggle.classList.add("toggle__active");
     }
-    // completeToggle.checked = element.isChecked; 
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "X";
     deleteButton.classList.add("button__delete");
     const p = document.createElement("p");
     p.textContent = element.value;
     p.classList.add("task__text");
-    p.addEventListener("dblclick", function() {
+
+    p.addEventListener("dblclick", function () {
       const input = document.createElement("input");
       input.value = p.textContent;
       p.parentNode.replaceChild(input, p);
       input.focus;
 
-      input.classList.add("input__task__change")
+      input.classList.add("input__task__change");
       input.focus;
-      input.addEventListener("blur", function() {
+      input.addEventListener("blur", function () {
         const edited = input.value;
         const p = document.createElement("p");
         input.parentNode.replaceChild(p, input);
-        const newText = renderData.find((item) => item.id === element.id) 
+        const newText = renderData.find((item) => item.id === element.id);
         if (edited) {
           newText.value = edited;
-          localStorage.setItem("array", JSON.stringify(initialData))
+          localStorage.setItem("array", JSON.stringify(initialData));
           p.classList.add("task__text");
-          render()
+          render();
         }
-      })
-    })
+      });
+    });
+
     const div = document.createElement("div");
     div.classList.add("task");
     div.prepend(completeToggle);
     div.append(p, deleteButton);
 
-
-    completeToggle.addEventListener("click", function () { 
+    completeToggle.addEventListener("click", function () {
       if (completeToggle.classList.contains("toggle__active")) {
         completeToggle.classList.remove("toggle__active");
         element.isChecked = false;
       } else {
-        completeToggle.classList.add("toggle__active")
+        completeToggle.classList.add("toggle__active");
         element.isChecked = true;
-    }
-    localStorage.setItem("array", JSON.stringify(initialData));
-    render()
+      }
+      localStorage.setItem("array", JSON.stringify(initialData));
+      render();
     });
 
     deleteButton.addEventListener("click", function () {
@@ -74,13 +76,14 @@ function render() {
       render();
       localStorage.setItem("array", JSON.stringify(initialData));
     });
-
+    const itemText = renderData.length === 1 ? "item" : "items"
+    taskCount.textContent = `${renderData.length} ${itemText} left`
     taskList.appendChild(div);
   });
 }
 
-checkAll.addEventListener('click', function () {
-  const allChecked = renderData.every((element) => element.isChecked)
+checkAll.addEventListener("click", function () {
+  const allChecked = renderData.every((element) => element.isChecked);
   if (allChecked) {
     renderData.forEach((element) => {
       element.isChecked = false;
@@ -93,7 +96,6 @@ checkAll.addEventListener('click', function () {
   localStorage.setItem("array", JSON.stringify(initialData));
   render();
 });
-
 
 completeCleanButton.addEventListener("click", function () {
   initialData = initialData.filter((item) => !item.isChecked);
@@ -127,47 +129,42 @@ function makeElement() {
 function addItem(newItem) {
   initialData.push(newItem);
   renderData = [...initialData];
+  filterItemsByType(filterType)
   render();
   localStorage.setItem("array", JSON.stringify(initialData));
   localStorage.setItem("objectId", objectId);
 }
 
+
+
+function filterItemsByType(Type) {
+  filterType = Type;
+
+
+  localStorage.setItem("filter-type", filterType);
+  console.log(filterType)
+  if (filterType === "completed__item") {
+    renderData = initialData.filter((item) => item.isChecked === true);
+  } else if (filterType === "active__item") {
+    renderData = initialData.filter((item) => item.isChecked === false);
+  } else {
+    renderData = initialData;
+  }
+  render();
+}
+
 const onFilterClick = (event) => {
   const target = event.target;
-  console.log(target)
-  console.log(target.dataset.type)
   const dataType = target.dataset.type;
-  
-  if(target.dataset.type === "complete__item") {
-    initialData = initialData.filter((item) => item.isChecked);
-    renderData = [...initialData];
-    console.log(renderData)
-    render();
-    localStorage.setItem("array", JSON.stringify(initialData));
-  } if (target.dataset.type === "active__item") {
-    initialData = initialData.filter((item) => !item.isChecked);
-    renderData = [...initialData];
-    console.log(renderData)
-    render();
-    localStorage.setItem("array", JSON.stringify(initialData));
-  } else {
-    console.log(renderData)
-    render();
-  }
-  // if (button.dataset.type) {
-  //   button.classList.add("filter__active");
-  // }
-  // filterButtons.forEach((button) => button.classList.remove("filter__active"))
-  // filterItemsByType(dataType);
-  // target.classList.add("active")
-}
+  filterButtons.forEach((button) => button.classList.remove("filter__active"))
+  filterItemsByType(dataType);
+  target.classList.add("filter__active")
+};
 
-function filterItemsByType() {
-
-}
 
 filterButtons.forEach((button) => {
-  button.addEventListener("click", onFilterClick)
-  button.classList.remove("filter__active")
-  console.log(button)
-});
+  button.addEventListener("click", onFilterClick);
+  if (button.dataset.type === filterType) {
+    button.classList.add("filter__active");
+  }
+}); 
