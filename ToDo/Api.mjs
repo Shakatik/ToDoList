9ff5843539c3eeb34bd4fd44329aa319293345ef
Shakatik <kiberking11@gmail.com>
2023-07-07 20:03:53 +0300

    Update Api.mjs

--- ToDo/Api.mjs
+++ ToDo/Api.mjs
@@ -1,3 +1,18 @@
-fetch('https://dummyjson.com/todos?limit=3')
-.then(res => res.json())
-.then(console.log);
+// fetch('https://dummyjson.com/todos?limit=3')
+// .then(res => res.json())
+// .then(console.log);
+
+// let apiData;
+
+// async function fetchData() {
+//     const response = await fetch('https://dummyjson.com/todos?limit=3');
+//     apiData = await response.json();
+//     console.log(apiData)
+// }
+
+
+
+
+// fetch('https://dummyjson.com/todos/1')
+// .then(res => res.json())
+// .then(console.log);
